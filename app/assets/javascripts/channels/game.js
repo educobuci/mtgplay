92f41a3b141835d99ab87c690f93337cb70ce51a
Eduardo Cobuci <educobuci@gmail.com>
2016-05-12 00:11:04 -0300

    Add speak method to game as a sample

--- app/assets/javascripts/channels/game.js
+++ app/assets/javascripts/channels/game.js
@@ -1,5 +1,14 @@
 App.game = App.cable.subscriptions.create("GameChannel", {
-  connected: function() {},
-  disconnected: function() {},
-  received: function(data) {}
+  connected: function() {
+    
+  },
+  disconnected: function() {
+    
+  },
+  received: function(data) {
+    alert(data.message)
+  },
+  speak: function(message){
+    return this.perform('speak', { message : message });
+  }
 });
--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,10 +1,14 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class GameChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from 'game_channel' 
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+  
+  def speak(data)
+    ActionCable.server.broadcast 'game_channel', message: data['message']
   end
 end
