7be18a3508fba94ca24fc31ef960d71555ddf0e0
Eduardo Cobuci <educobuci@gmail.com>
2016-05-23 00:16:49 -0300

    Very first interface

--- app/assets/javascripts/channels/game.js
+++ app/assets/javascripts/channels/game.js
@@ -1,14 +1,18 @@
 App.game = App.cable.subscriptions.create("GameChannel", {
   connected: function() {
-    
+    console.log("Waiting for another player...")
   },
   disconnected: function() {
     
   },
   received: function(data) {
-    alert(data.message)
+    //alert(data.message)
+    console.log(data)
   },
   speak: function(message){
-    return this.perform('speak', { message : message });
+    return this.perform('speak', { message: message });
+  },
+  move: function(method, args) {
+    return this.perform('move', { method: method, args: args })
   }
 });
--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,7 +1,9 @@
-# Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
+require "mtgsim"
+
 class GameChannel < ApplicationCable::Channel
   def subscribed
-    stream_from 'game_channel' 
+    stream_from "player_#{uuid}"
+    GameServer.seek uuid
   end
 
   def unsubscribed
@@ -9,6 +11,62 @@ class GameChannel < ApplicationCable::Channel
   end
   
   def speak(data)
-    ActionCable.server.broadcast 'game_channel', message: data['message']
+    ActionCable.server.broadcast "player_#{uuid}", message: data['message']
+  end
+  
+  def action(data)
+    args = data['args'] ? [@index, data['args']].flatten : @index
+    puts "ARGS: " + args.to_s
+    puts (game.send data['method'], *args)
+  end
+  def game
+    GameServer.instance
+  end
+end
+
+class GameServer
+  @@seeks = nil
+  @@instance = nil
+  
+  def initialize(players)
+    @players = players.map do |id|
+      player = Player.new
+      player.id = id
+      player
+    end
+    @game = Game.new @players
+    @game.add_observer self
+  end
+  def start
+    @game.roll_dices
+    @game.start_player @game.die_winner, @game.die_winner
+    @game.draw_hands
+    @game.keep @game.die_winner
+    die_loser = @game.die_winner == 0 ? 1 : 0
+    @game.keep die_loser
+    @game.start
+    @players.each do |p|
+      ActionCable.server.broadcast "player_#{p.id}", game_start: p.id, hand: p.hand.to_json
+    end
+  end
+  def update(status, value)
+    puts "UPDATE: #{status} - #{value}"
+    @players.each do |p|
+      ActionCable.server.broadcast "player_#{p.id}", status => value
+    end
+  end
+  
+  # Class methods
+  def self.seek(uuid)
+    if @@seeks
+      @@instance = GameServer.new [@@seeks, uuid]
+      @@seeks = nil
+      @@instance.start
+    else
+      @@seeks = uuid
+    end
+  end
+  def instance
+    @@instance
   end
 end
